58ebdbb6aad507fa7521a7ea1695ed844ac755a8
michelyjiang@hotmail.com <michelyjiang@hotmail.com>
2016-06-11 14:13:05 -0400

    adding small change

--- app/src/main/java/com/example/michel/arg/MainActivity.java
+++ app/src/main/java/com/example/michel/arg/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.michel.arg;
 import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.RelativeLayout;
@@ -23,9 +24,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         modelViewController();
