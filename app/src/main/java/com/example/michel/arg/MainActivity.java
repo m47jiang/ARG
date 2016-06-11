b50b41bb4e352cca360539eea9f4fde175ec95a8
George Shen <george.school303@gmail.com>
2016-06-11 14:10:11 -0400

    Camera debug

--- app/src/main/java/com/example/michel/arg/MainActivity.java
+++ app/src/main/java/com/example/michel/arg/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.michel.arg;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.RelativeLayout;
@@ -36,7 +39,26 @@ public class MainActivity extends AppCompatActivity {
         model = new Model(this);
         homeView = new HomeView(this);
         controller = new Controller(this);
+
+        // DEBUG
+        boolean camAvailable = checkCameraHardware(this);
+
+        if(!camAvailable) {
+            Log.d("cameraAvailability", "There is no camera available :(");
+        }
+
         camView = new CameraView(this, getCameraInstance());
+    }
+
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
     }
 
     /** A safe way to get an instance of the Camera object. */
