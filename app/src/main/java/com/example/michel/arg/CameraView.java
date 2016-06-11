c2e96c3c66057b60f2540fb7cc62fc3e07632a34
George Shen <george.school303@gmail.com>
2016-06-11 13:46:34 -0400

    Preview the camera

--- app/src/main/java/com/example/michel/arg/CameraView.java
+++ app/src/main/java/com/example/michel/arg/CameraView.java
@@ -1,27 +1,73 @@
 package com.example.michel.arg;
 
+import android.content.Context;
 import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.widget.RelativeLayout;
+
+import java.io.IOException;
 
 /**
  * Created by michel on 11/06/16.
  */
-public class CameraView extends RelativeLayout {
+public class CameraView extends SurfaceView implements SurfaceHolder.Callback {
+    private SurfaceHolder mHolder;
+    private Camera mCamera;
 
-    private MainActivity mainActivity;
-    private Model model;
+    public CameraView(Context context, Camera camera) {
+        super(context);
+        mCamera = camera;
 
-    public CameraView(MainActivity mainActivity) {
-        super(mainActivity);
-        this.mainActivity = mainActivity;
-        this.model = mainActivity.getModel();
-        startCamera();
-        //initAppView();
-        //updateUILoop();
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        // deprecated setting, but required on Android versions prior to 3.0
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
     }
 
-    private void startCamera() {
-        Camera object = null;
-        object = Camera.open();
+    public void surfaceCreated(SurfaceHolder holder) {
+        // The Surface has been created, now tell the camera where to draw the preview.
+        try {
+            mCamera.setPreviewDisplay(holder);
+            mCamera.startPreview();
+        } catch (IOException e) {
+            Log.d("ioexception", "Error setting camera preview: " + e.getMessage());
+        }
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // empty. Take care of releasing the Camera preview in your activity.
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+
+        if (mHolder.getSurface() == null){
+            // preview surface does not exist
+            return;
+        }
+
+        // stop preview before making changes
+        try {
+            mCamera.stopPreview();
+        } catch (Exception e){
+            // ignore: tried to stop a non-existent preview
+        }
+
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+
+        // start preview with new settings
+        try {
+            mCamera.setPreviewDisplay(mHolder);
+            mCamera.startPreview();
+
+        } catch (Exception e){
+            Log.d("exception", "Error starting camera preview: " + e.getMessage());
+        }
     }
 }
--- app/src/main/java/com/example/michel/arg/MainActivity.java
+++ app/src/main/java/com/example/michel/arg/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.michel.arg;
 
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private Model model;
     private Controller controller;
     private HomeView homeView;
+    private CameraView camView;
 
     //original code
     /*@Override
@@ -27,13 +29,26 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         modelViewController();
-        setContentView(homeView);
+        setContentView(camView);
     }
 
     private void modelViewController() {
         model = new Model(this);
         homeView = new HomeView(this);
         controller = new Controller(this);
+        camView = new CameraView(this, getCameraInstance());
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
     }
 
     public Model getModel() {
