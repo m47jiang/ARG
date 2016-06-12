cd29c258ef0032e46b54ac5eaa81a77ff2ebca4d
michelyjiang@hotmail.com <michelyjiang@hotmail.com>
2016-06-11 23:44:03 -0400

    camera send is working yeasss

--- app/src/main/java/com/example/michel/arg/Controller.java
+++ app/src/main/java/com/example/michel/arg/Controller.java
@@ -5,9 +5,26 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.hardware.Camera;
 import android.media.MediaPlayer;
+import android.renderscript.Byte3;
 import android.util.Log;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  * Created by michel on 11/06/16.
@@ -25,7 +42,7 @@ public class Controller {
 
 
     public Controller(Context context) {
-        this.mainActivity = (MainActivity)context;
+        this.mainActivity = (MainActivity) context;
         this.model = mainActivity.getModel();
         this.homeView = mainActivity.getHomeView();
         //createBitmaps();
@@ -37,37 +54,149 @@ public class Controller {
     }
 
     public void takePicture() {
-        camera = mainActivity.getCameraInstance();
+        camera = MainActivity.getCameraInstance();
 
-        //camera.takePicture(null,null,null,null);
         Log.e("cameraexception", "took a picture");
 
         //camera.startPreview();
-        camera.takePicture(null,null,null, new Camera.PictureCallback(){
+        camera.takePicture(null, null, null, new Camera.PictureCallback() {
 
             @Override
             public void onPictureTaken(byte[] data, Camera camera) {
                 //BitmapFactory.Options options=new BitmapFactory.Options();
                 //options.inSampleSize = 5;
                 //image=BitmapFactory.decodeByteArray(data,0,data.length,options);
+                try {
+                    sendImage(data);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 camera.startPreview();
             }
         });
         Log.e("cameraexception", "took a picture");
     }
-
-    public void changeSubtitle (TextView sub){
+    public void changeSubtitle (TextView sub) {
         int i = 0;
-        for(; i < model.subtitles.length; i++){
+        for (; i < model.subtitles.length; i++) {
             if (sub.getText().equals(model.subtitles[i]))
                 break;
         }
 
-        if (i != model.subtitles.length-1)
-            sub.setText(model.subtitles[i+1]);
+        if (i != model.subtitles.length - 1)
+            sub.setText(model.subtitles[i + 1]);
         else {
             sub.setText("Game Over");
             mp.stop();
         }
     }
+
+    public void sendImage(final byte[] data) throws IOException {
+
+        Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+        Bitmap scale = Bitmap.createScaledBitmap(bmp, bmp.getWidth() / 5, bmp.getHeight() / 5, true);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        scale.compress(Bitmap.CompressFormat.JPEG, 80, outputStream);
+        final byte[] newdata = outputStream.toByteArray();
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(1000);
+                    OkHttpClient client = new OkHttpClient();
+
+                    MediaType mediaType = MediaType.parse("image/jpeg");
+                    RequestBody filebody = RequestBody.create(mediaType, newdata);
+                    MultipartBody body = new MultipartBody.Builder().addFormDataPart("file", "whatever.jpg", filebody).build();
+                    Request request = new Request.Builder()
+                            .url("https://api.havenondemand.com/1/api/sync/ocrdocument/v1?apikey=b14a0613-91b1-4485-9b9b-41225643cb94&mode=scene_photo")
+                            .post(body)
+                            .addHeader("cache-control", "no-cache")
+                            .build();
+
+                    Response response = client.newCall(request).execute();
+                    String responseBody = response.body().string();
+                    String mob = response.message();
+                    /*OkHttpClient client = new OkHttpClient();
+                    String url = "https://api.havenondemand.com/1/api/sync/ocrdoocument/v1";
+
+                    RequestBody body = RequestBody.;
+                    //MultipartBody body = new MultipartBody.Builder().addFormDataPart("name", "whatever.jpg", RequestBody.create(MediaType.parse("image/jpeg"), data)).build();
+                    //Request request = new Request.Buildeir().url(url).post(body).build();
+
+                    Request request = new Request.Builder()
+                            .url(url)
+                            .addHeader("url", "http://www.brainyquote.com/photos/a/abrahamlincoln137180.jpg")
+                            .addHeader("apikey", "b14a0613-91b1-4485-9b9b-41225643cb94")
+                            .build();//                            .post()
+                    Response response = client.newCall(request).execute();
+                    String mob = response.message();
+                    //return response.body().string();*/
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        };
+        thread.start();
+    }
 }
+
+/*
+        final HttpURLConnection[] client = {null};
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(1000);
+                    try {
+                        URL url = new URL("https://api.havenondemand.com/1/api/sync/ocrdocument/v1");
+                        client[0] = (HttpURLConnection) url.openConnection();
+                        client[0].setRequestMethod("POST");
+                        client[0].setRequestProperty("URL", "http://i0.wp.com/dailynous.com/wp-content/uploads/2015/02/motivational-sidgwick.png");
+                        client[0].setRequestProperty("apikey", "b14a0613-91b1-4485-9b9b-41225643cb94");
+                        client[0].setDoOutput(true);
+
+                        OutputStream outputPost = new BufferedOutputStream(client[0].getOutputStream());
+                        check(outputPost);
+                        outputPost.flush();
+                        outputPost.close();
+
+
+                    } catch (MalformedURLException error) {
+                        Log.e("MALFORMEDURLEXCEPTION", "problem with" + error);
+                        //Handles an incorrectly entered URL
+                    } catch (SocketTimeoutException error) {
+                        Log.e("SOCKETIMEOUTEXCEPTION", "problem with" + error);
+//Handles URL access timeout.
+                    } catch (IOException error) {
+                        Log.e("IOEXCEPTION", "problem with" + error);
+//Handles input and output errors
+                    } finally {
+                        if (client[0] != null) // Make sure the connection is not null.
+                            client[0].disconnect();
+                    }
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+
+        thread.start();
+
+
+    }
+
+    private void check(OutputStream outputPost) {
+        BufferedOutputStream baos = (BufferedOutputStream) outputPost;
+        String result = outputPost.toString();
+    }
+
+}*/
